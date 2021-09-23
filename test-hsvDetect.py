9354998164f442057aba0f5dc874655bd7231001
HabiJung <habijungx@gmail.com>
2021-09-23 18:08:52 +0900

    Update code test > HSV Detect

--- test-hsvDetect.py
+++ test-hsvDetect.py
@@ -6,31 +6,39 @@ def onChange(pos):
     pass
 
 src = "car-lime.jpg"
-barName  = ["Hue Min", "Hue Max", "Sat Min", "Sat Max", "Val Min", "Val Max"]
-barVal   = [42, 63, 96, 255, 145, 255]
-barMax   = [179, 179, 255, 255, 255, 255]
 
-cv2.namedWindow("HSV Bar")
-cv2.resizeWindow("HSV Bar", 500, 500)
+
+''' Create HSV Trackbar '''
+title = "HSV Detector"
+hsvParameter  = ["Hue Min", "Hue Max", "Sat Min", "Sat Max", "Val Min", "Val Max"]
+hsvInit       = [42, 63, 96, 255, 145, 255]
+cv2.namedWindow(title)
+cv2.resizeWindow(title, 500, 400)
 
 for i in range(6):
-    cv2.createTrackbar(barName[i], "HSV Bar", barVal[i], barMax[i], onChange)
+    if (i < 2):
+        cv2.createTrackbar(hsvParameter[i], title, hsvInit[i], 179, lambda x: x)
+    else: 
+        cv2.createTrackbar(hsvParameter[i], title, hsvInit[i], 179, lambda x: x)
 
+
+''' Show Result '''
 while cv2.waitKey(1) != ord('q'):
+
     img = cv2.imread(src)
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+    ''' Get Trackbar Position '''
     for i in range(6):
-        barVal[i] = cv2.getTrackbarPos(barName[i], "HSV Bar")
+        hsvInit[i] = cv2.getTrackbarPos(hsvParameter[i], title)
 
-    lower = np.array([barVal[0], barVal[2], barVal[4]])
-    upper = np.array([barVal[1], barVal[3], barVal[5]])
-    mask = cv2.inRange(imgHSV, lower, upper)
-    imgResult = cv2.bitwise_and(img, img, mask=mask)
+    lower = np.array([hsvInit[0], hsvInit[2], hsvInit[4]])
+    upper = np.array([hsvInit[1], hsvInit[3], hsvInit[5]])
 
-    imgStack = ftn.stackImages(0.7, ([img, imgHSV], [mask, imgResult]))
+    imgMask = cv2.inRange(imgHSV, lower, upper)
+    imgResult = cv2.bitwise_and(img, img, mask=imgMask)
+    imgStack = ftn.stackImages(0.7, ([img, imgHSV], [imgMask, imgResult]))
 
-    # cv2.imshow("Result", imgStack)
-    cv2.imshow("HSV Bar", imgStack)
+    cv2.imshow(title, imgStack)
 
 cv2.destroyAllWindows()
